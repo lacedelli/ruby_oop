1debed305eaa26bb9cbb20365efe58ba46ea032e
Lacedelli <lacedelli@outlook.com>
2019-11-21 15:53:43 -0600

    change some code around, add comment outline for the program

--- tic_tac/board.rb
+++ tic_tac/board.rb
@@ -1,14 +1,45 @@
 class Board
-	attr_accessor :top, :middle, :bottom
+	attr_reader :top, :middle, :bottom
 	def initialize()
+		# Set up the board, each row assigned to a different variable
 		@top = {:right => " ",
 						:center => " ",
 						:left => " "}
 		@middle = {:right => " ",
 						   :center => " ",
 							 :left => " "}
-		@bottom = {:rigt => " ",
+		@bottom = {:right => " ",
 						   :center => " ",
 							 :left => " "}
 	end
+
+	# Set a value to the selected cel	
+	def set_value
+		#TODO check if selected cel is valid
+		#TODO access cel with new value
+		#TODO return true
+		#TODO if cel is not valid
+		#TODO print message telling the user so
+		#TODO return false
+	end
+	# Check if any of the lines is complete
+	def line_complete?(char)
+		#TODO	define a character to check
+		#TODO Check each row
+		#TODO Check each column
+		#TODO Chect diagonal rows
+	end
+	# Override to_s to make it easier to print
+	def to_s() 
+		"|#{@top[:left]}|#{@top[:center]}|#{@top[:right]}|\n"\
+		"|—————|\n"\
+		"|#{@middle[:left]}|#{@middle[:center]}|#{@middle[:right]}|\n"\
+		"|—————|\n"\
+		"|#{@bottom[:left]}|#{@bottom[:center]}|#{@bottom[:right]}|\n"
+	end
+	
+
+	private
+	# Set up the writers as private methods
+	attr_writer :top, :middle, :bottom
 end
--- tic_tac/game.rb
+++ tic_tac/game.rb
@@ -7,25 +7,38 @@ class Game
 		# set up the round counter for each player
 		@p1_rounds = 0
 		@p2_rounds = 0
-		# Load the board class
+		# Player turn to be decided with modulo op.
+		@player_turn = 0
+		# Instatiate board
 		@board = Board.new()
 
 	end
 	# Override to_s to print the board.
 	def to_s()
-		p_board()
+		@board.to_s
 	end
-
+	# Method that plays a round of the game
+	def play_round()
+		# TODO Figure out which player moves and assign character depending on that
+		# TODO Ask the player for a move
+		# TODO Check if the move is valid by confirming it is an empty space
+		# TODO Make the move
+		# TODO add to player counter, changing turn
+	end
+	def play_game()
+		#TODO print a message welcoming players
+		#TODO start a loop 
+		#TODO play a round
+		#TODO check for win condition
+		#TODO check for stalemate
+		#TODO end loop if win or stalemate
+		#TODO increment winner's score
+		#TODO ask if players want another go
+		#TODO set to true if yes
+		#TODO print scores and let program end if not
+	end	
 	private
-	# The helper method that prints the board
-	def p_board
-		"#{board.top[:left]}|#{board.top[:center]}|#{board.top[:left]}\n"\
-		 "——————\n"\
-		 "#{board.middle[:left]}|#{board.middle[:center]}|#{board.middle[:left]}\n"\
-		 "——————\n"\
-		 "#{board.bottom[:left]}|#{board.bottom[:center]}|#{board.bottom[:left]}\n"
-	end
-
+	attr_accessor :player_turn
 end
 
 game = Game.new()
