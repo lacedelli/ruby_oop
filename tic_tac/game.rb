49df3ff44e6e5c85e38076a58dd22c530b899293
Lacedelli <lacedelli@outlook.com>
2019-11-27 17:22:56 -0600

    created a branch to get the Game class working correctly

--- tic_tac/game.rb
+++ tic_tac/game.rb
@@ -16,21 +16,17 @@ class Game
 	def to_s()
 		@board.to_s
 	end
+
 	# Method that plays a round of the game
-	def play_round()
-		# Figure out which player moves and assign character depending on that
-		if self.player_turn % 2 == 0
-			char = "X"
-		else
-			char = "O"
-		end
+	def play_round(char)
+		char = char
 		puts "It's #{char}'s turn!"
 		# Ask the player for a move
 		puts self.to_s()
 		puts "Please write your move, the options are:\n"\
 				 "'top', 'middle' and 'bottom' for the rows, and\n"\
 				 "'left', 'center', and 'right' for the columns.\n"\
-				 "Please write them with a space between words, like so: 'top right'."
+				 "Please write them with a space between words, like so: 'top right'.\n"
 		move = gets.split()
 		# Check for typos in the answer
 		# TODO find a way to encapsulate this code in such a way that whenever
@@ -39,7 +35,7 @@ class Game
 		# and the user gets the correct message on each step.
 		while @board.understands?(move[0], move[1]).include?(false)
 			puts "I'm sorry, I could not understand the move,\n"\
-					 "Could you please write it again for me?"
+					 "Could you please write it again for me?\n"
 			move = gets.split()
 		end
 		# Check if the move is valid by confirming it is an empty space
@@ -51,25 +47,22 @@ class Game
 		# add to player counter, changing turn
 		self.player_turn += 1
 	end
-	def play()
-		if self.player_turn % 2 == 0
-			char = "X"
-		else
-			char = "O"
-		end
-		#print a message welcoming players
-		puts "Let's play tic-tac-toe!"
+
+	# Play a single match of the game
+	def play_match()
+		char = get_player()
 		#start a loop 
 		player_won = false
 		while not player_won
 			#play a round
-			self.play_round()
+			self.play_round(char)
 			#check for win condition
 			if self.board.line_complete?(char)
 				if char == "X"
 					puts "Player 1 won!"
 					self.p1_rounds += 1
 				else
+					# TODO find out why the game doesn't end here
 					puts "Player 2 won!"
 					self.p2_rounds += 1
 				end
@@ -78,22 +71,46 @@ class Game
 			end
 			#TODO check for stalemate
 		end	
-		#ask if players want another go
-		puts "Do you want to play another round? Y/N"
-		if gets.chomp.downcase == "y" || gets.chomp.downcase == "yes"
-			# TODO maybe calling itself would work better? 
-			# I'm honestly thinking in game engine terms, where I'm assuming
-			# that the loop is gonna pick up where I left off.
-			player_won = false
-		else
-			puts "Thank you for playing! The final scores were:\n"\
-					 "Player 1: #{p1_rounds}, Player 2: #{p2_rounds}."
-		end
-	end	
+			end	
 	 
+	def play_game()
+		#print a message welcoming players
+		puts "Let's play tic-tac-toe!"
+
+		continue_game = true
+		while continue_game
+			play_match()
+			#ask if players want another go
+			puts "Do you want to play another round? Y/N"
+			if gets.chomp.downcase == "y" || gets.chomp.downcase == "yes"
+				# Create a new board for the new game
+				self.board = Board.new()
+				play_match()
+			else
+				puts "Thank you for playing! The final scores were:\n"\
+						 "Player 1: #{p1_rounds}, Player 2: #{p2_rounds}."
+				continue_game = false
+			end
+		end
+
+	end
+
 	private
 	attr_writer :player_turn, :p1_rounds, :p2_rounds, :board
+
+	def get_player()
+		if self.player_turn % 2 == 0
+			return "X"
+		else
+			return "O"
+		end
+	end
+
+	def get_move()
+
+	end
+
 end
 
 game = Game.new
-game.play
+game.play_game
