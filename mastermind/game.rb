6e3d6fb5eeb82949d28907ad7c3d0f89d21cf46e
Lacedelli <lacedelli@outlook.com>
2019-12-01 13:46:49 -0600

    add a skeleton for the intended workflow of board class

--- mastermind/game.rb
+++ mastermind/game.rb
@@ -1,2 +1,25 @@
 class Game
+	def initialize()
+		# TODO set up players
+		# TODO set up board
+		# TODO create variable for guesses
+	end
+
+	def play_round()
+		# TODO restart variable for guesses
+		# TODO Set up a loop with guesses as a limit
+			# TODO Get a guess from active player
+			# TODO Compare guess with current board
+			# TODO assign points to each player
+			# TODO if guess is correct, end loop
+		# TODO if loop ends, end game and assign points
+	end
+
+	def play_game()
+		# TODO set up a loop
+			# TODO play a round
+			# TODO print scores, ask if player wants to play another round
+			# TODO if player says yes, continue loop
+			# TODO Else, break
+	end
 end
