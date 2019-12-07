484799d3861f2fda5933fbf0a9d23b698113ab2f
Lacedelli <lacedelli@outlook.com>
2019-12-06 18:26:50 -0600

    add tentative working code to the board file

--- mastermind/board.rb
+++ mastermind/board.rb
@@ -1,21 +1,49 @@
 class Board
 	def initialize() 
+		attr_reader :secret_code
 		# Set a variable that holds four values
-		@secret_code = Hash.new()
+		@secret_code = Array.new()
 	end
 
 	def create_board(array)
-		# TODO Grab the values contained in the array passed to the board
-		# TODO and assign them in order to the values in @secret_code 
+		# Grab the values contained in the array passed to the board
+		# and assign them in order to the values in @secret_code 
+		values = array
+
+		values.each_index do |v, i|
+			case i.downcase
+			when "red"
+				self.secret_code[i] = "R"
+			when "blue"
+				self.secret_code[i] = "B"
+			when "yellow"
+				self.secret_code[i] = "Y"
+			when "green"
+				self.secret_code[i] = "G"
+			end
+		end
 	end
 
 	def asses_guess(guess)
-		# TODO Make an array to push the status of the guesses
-		# TODO Compare the guess array with each of the values in @secret_code
-		# TODO Send a red signal if the guess is wrong
-		# TODO Send a blue signal if the the guess is right, but in the wrong place
-		# TODO Send a green signal if the guess is right
-		# TODO return signals
+		# Make an array to push the status of the guesses
+		correct_guesses = Array.new
+		# Make an array that we can pop values from
+		local_code = self.secret_code
+		# Compare the guess array with each of the values in @secret_code
+		local_code.each_index do |v, i|
+		# Send a green signal if the guess is right
+			if guess[i] == v
+				correct_guesses.push("G")
+				local_code[i] = nil
+		# Send a blue signal if the the guess is right, but in the wrong place
+			elsif local_code.include?(guess[i])
+				correct_guesses.push("B")
+				local_code[i] = nil
+			else
+				correct_guesses.push("R")
+				local_code[i] = nil
+		end
+		correct_guesses	
 	end
 
 	def to_s()
@@ -23,6 +51,11 @@ class Board
 		# TODO within @secret_code and changes the colors of each letter
 		# TODO depending on the value
 	end
+
+	def colorize(string)
+	end
 	
+	private
+	attr_writer :secret_code
 end
 
