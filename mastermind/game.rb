5f1dc26e058f2fa2044bcdf863194af3567c1b4c
Lacedelli <lacedelli@outlook.com>
2019-12-12 12:50:30 -0600

    add bits of code for basic functionality

--- mastermind/game.rb
+++ mastermind/game.rb
@@ -1,20 +1,28 @@
+require_relative "players.rb"
 require_relative "board.rb"
 class Game
+	attr_reader :board, :pc, :npc
+
 	def initialize()
 		# TODO set up players
+		pc = Player.new()
+		npc = ComputerPlayer.new()
 		# Set up board
 		@board = Board.new()
 		# create variable for guesses
 		@guesses = Array.new()
 	end
 
-	def play_round()
+	def play_round(guesses_num)
 		# TODO restart variable for guesses
+		guesses = []
 		# TODO Set up a loop with guesses as a limit
+		guesses_num.each do
 			# TODO Get a guess from active player
 			# TODO Compare guess with current board
 			# TODO assign points to each player
 			# TODO if guess is correct, end loop
+		end
 		# TODO if loop ends, end game and assign points
 	end
 
@@ -25,4 +33,7 @@ class Game
 			# TODO if player says yes, continue loop
 			# TODO Else, break
 	end
+	
+	private 
+	attr_writer :board, :pc, :npc
 end
