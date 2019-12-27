3eb7022039d5e0a67711961f906e5bff95666fce
Lacedelli <lacedelli@outlook.com>
2019-12-27 11:49:41 -0600

    removed redundant player code

--- mastermind/players.rb
+++ mastermind/players.rb
@@ -4,28 +4,30 @@ class Player
 	end
 
 	def get_code(spaces)
-		guesses = spaces.length()
+		guesses = spaces
 		code = Array.new
 		# Make a loop
 		loop do
 			# ensure that the input is valid for the game
-			puts "Make your guess! You've got #{guesses} left"
+			puts "Select a color! You've got #{guesses} left"
 			puts "The options are: Green, Red, Blue, Yellow"
 			color = gets().chomp()
 			# If valid, append to the array
 			if self.valid_input?(color)
 				code.push(color)
+				guesses -= 1
 			else
 				puts "The input was not valid, please check for typos"
-				guesses -= 1
 				next
 			end
-			if guesses == code.length()
+			if guesses == 0
 				break
 			end
 		end
 		code
 	end
+
+	private
 
 	def valid_input?(input)
 		color = input
@@ -43,29 +45,9 @@ class Player
 		end
 	end
 
-	def make_guess(spaces)
-		board_spaces = spaces
-		guesses = Array.new()
-		loop do
-			puts "Make your guess! you have #{board_spaces - guesses.length()} left!"
-			puts "Your options are red, green, blue and yellow."
-			guess = gets().chomp()
-			if self.valid_guess?(guess)
-				guesses.push(guess)
-			else
-				puts "I could not understand the text, please check for typos"
-				next
-			end
-			if guesses.length() == board_spaces
-				break
-			end
-		end
-		guesses
-	end
-
 end
 
-class ComputerPlayer > Player
+class ComputerPlayer < Player
 
 	def initialize()
 		# Initialize array for correct guesses
@@ -100,8 +82,7 @@ class ComputerPlayer > Player
 		# TODO return guesses
 	end	
 
-	def asses_guess()
-		# TODO get values from board and parse them with regexp
+	def asses_guess(results)
 		# TODO Check if any of the values equals to green and add them
 		# TODO to the respective slot on the instance variable for next guesses
 	end
