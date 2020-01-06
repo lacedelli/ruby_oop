55e1af0a8dd236084881eb1f60d7a14702983d67
Lacedelli <lacedelli@outlook.com>
2020-01-06 13:33:30 -0600

    add code that gets the game into a near-playable state, the only things missing are figuring out the reason that the game does not add scores for the players and board.all_answers_correct? not working correctly as a break

--- mastermind/game.rb
+++ mastermind/game.rb
@@ -1,7 +1,7 @@
 require_relative "players.rb"
 require_relative "board.rb"
 class Game
-	attr_reader :board, :pc, :npc, :code_length, :player_switch, :current_guess
+	attr_reader :board, :pc, :npc, :player_switch, :player_scores
 
 	def initialize()
 		# TODO set up players
@@ -10,14 +10,14 @@ class Game
 		# Set up board
 		@board = Board.new()
 		# create variable for guesses
-		@code_length 
 		@player_switch = 0
-		@current_guess = Array.new()
+		@player_scores = {:pc => 0, :npc => 0}
 	end
 
 	def play_round(guesses_num)
 		# TODO restart variable for guesses
 		guesses = []
+		score = 0
 		# TODO Set up a loop with guesses as a limit
 		guesses_num.times do |i|
 			# TODO Get a guess from active player
@@ -29,12 +29,24 @@ class Game
 			end
 			puts self.board.assess_guess(guesses, true)
 			if self.board.all_guesses_correct?(guesses)
+				puts "all guesses correct true"
 				break
 			end
 		end
 		puts "Round ended!"
-		
-		# TODO if loop ends, end game and assign points
+		# If loop ends, end game and assign points
+		guesses = self.board.assess_guess(guesses)
+		guesses.map do |guess|
+			if guess == "g"
+				score += 1
+			end
+		end
+
+		if self.player_switch % 2 == 0
+			self.player_scores[:npc] += score
+		else
+			self.player_scores[:pc] += score
+		end
 	end
 
 	def play_game()
@@ -43,6 +55,27 @@ class Game
 		spaces = 0 
 		while spaces <= 0 || nil
 			spaces = gets.chomp().to_i()
+		end
+		if self.player_switch % 2 == 0
+			self.board.create_board(self.npc.get_code(spaces))
+		else
+			puts "It's your turn to make a code!"
+			self.board.create_board(self.pc.get_code(spaces))
+		end
+
+		# set up a loop
+		loop do
+			# TODO play a round
+			self.play_round(spaces)	
+			# TODO print scores, ask if player wants to play another round
+			puts "The current scores are #{self.player_scores[:pc]} points for you, and #{self.player_scores[:npc]} for the \"computer.\""
+			# TODO if player says yes, continue loop
+			puts "Do you want to play another round? Y/N"
+			answer = gets().chomp()
+			unless /[yY]/.match?(answer)
+				break
+			end
+			self.player_switch += 1
 			if self.player_switch % 2 == 0
 				self.board.create_board(self.npc.get_code(spaces))
 			else
@@ -50,16 +83,8 @@ class Game
 				self.board.create_board(self.pc.get_code(spaces))
 			end
 		end
-		# set up a loop
-		loop do
-			# TODO play a round
-			self.play_round(spaces)	
-			# TODO print scores, ask if player wants to play another round
-			# TODO if player says yes, continue loop
-			# TODO Else, break
-		end
 	end
 	
 	private 
-	attr_writer :board, :pc, :npc, :code_length, :player_switch, :current_guesses
+	attr_writer :board, :pc, :npc, :player_switch, :player_scores
 end
